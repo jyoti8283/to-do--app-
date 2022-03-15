3186493934fc894aacc83f32c751dc3816386944
jyoti8283 <jyotisingh392003@gmail.com>
2022-03-16 02:08:28 +0530

    some changes are done in todoapp

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-let addToButton =document.getElementById("addtoo");
+let addToButton = document.getElementById("addtoo");
 let toDoContainer = document.getElementById("toDoContainer");
 let inputfeild = document.getElementById("inputfield");
-let addToclearButton =document.getElementById("clear");
+let addToclearButton = document.getElementById("clear");
 
- 
- addToButton.addEventListener('click',function() {
-     var paragragh =document.createElement('p');
-     paragragh.classList.add("paragragh-styling")
-     paragragh.innerText =inputfeild.value;
-     toDoContainer.appendChild(paragragh)
-     inputfeild.value ="";
-    //  paragragh.addEventListener('click', function(){
-        // toDoContainer.removeChild(paragragh);
-//   })
-addToclearButton.addEventListener('click',function(){
-            toDoContainer.removeChild(paragragh);
+addToButton.addEventListener("click", function () {
+  var paragragh = document.createElement("p");
+  paragragh.classList.add("paragraph-styling");
+  paragragh.innerText = inputfeild.value;
+  toDoContainer.appendChild(paragragh);
+  inputfeild.value = "";
 
-    
-})
+  paragragh.addEventListener("click", function () {
+    paragragh.contentEditable = true;
+  });
 
- })
+  //  paragragh.addEventListener('click', function(){
+  // toDoContainer.removeChild(paragragh);
+
+  addToclearButton.addEventListener("click", function () {
+    toDoContainer.removeChild(paragragh);
+  });
+});
