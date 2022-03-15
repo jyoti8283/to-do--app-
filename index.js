d790ba8cb5e045021f011247dc6bd5bead8a7dd1
jyoti8283 <jyotisingh392003@gmail.com>
2022-03-15 17:15:25 +0530

    somes changes are done in todoapp

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let addToButton =document.getElementById("addtoo");
 let toDoContainer = document.getElementById("toDoContainer");
 let inputfeild = document.getElementById("inputfield");
+let addToclearButton =document.getElementById("clear");
+
  
  addToButton.addEventListener('click',function() {
      var paragragh =document.createElement('p');
@@ -8,8 +10,13 @@ let inputfeild = document.getElementById("inputfield");
      paragragh.innerText =inputfeild.value;
      toDoContainer.appendChild(paragragh)
      inputfeild.value ="";
-     paragragh.addEventListener('click', function(){
-        toDoContainer.removeChild(paragragh);
-  })
+    //  paragragh.addEventListener('click', function(){
+        // toDoContainer.removeChild(paragragh);
+//   })
+addToclearButton.addEventListener('click',function(){
+            toDoContainer.removeChild(paragragh);
+
+    
+})
 
  })
